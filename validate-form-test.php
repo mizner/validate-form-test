078cb6b5de368e288d51a41470864a699b94af08
Michael Mizner <michaelmizner@gmail.com>
2017-12-06 15:41:49 -0500

    Main plugin file update
    Instantiated Shortcode and autoloader

--- validate-form-test.php
+++ validate-form-test.php
@@ -16,3 +16,8 @@ defined('WPINC') || die;
 define(__NAMESPACE__ . '\PATH', plugin_dir_path(__FILE__));
 define(__NAMESPACE__ . '\URI', plugin_dir_url(__FILE__));
 
+require_once 'lib/autoload.php';
+
+add_action('plugins_loaded', function () {
+    new Shortcode\Form();
+});
