9065b69c0c6761bd5bb3739f8fb9e33e09ccacba
Michael Mizner <michaelmizner@gmail.com>
2017-12-06 15:42:09 -0500

    Form template

---
+++ templates/form.php
@@ -0,0 +1,7 @@
+<form action="">
+    <input type=text data-type="email">
+    <input type=text data-type="first">
+    <input type=text data-type="last">
+    <input type=text data-type="zip">
+    <input type=text data-type="birthday">
+</form>
