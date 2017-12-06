f37cb8b749f41bb30c70fe0fe7c31f1a7c5590af
Michael Mizner <michaelmizner@gmail.com>
2017-12-06 15:39:35 -0500

    Plugin Setup

--- validate-form-test.php
+++ validate-form-test.php
@@ -1,1 +1,18 @@
 <?php
+/**
+ * Plugin Name: Validate Form Test
+ * Plugin URI: http://mizner.io
+ * Description:
+ * Version: 1.0
+ * Author: Michael Mizner
+ * Author URI: http://mizner.io
+ * License: GPLv2+
+ */
+
+namespace Mizner\VFT;
+
+defined('WPINC') || die;
+
+define(__NAMESPACE__ . '\PATH', plugin_dir_path(__FILE__));
+define(__NAMESPACE__ . '\URI', plugin_dir_url(__FILE__));
+
